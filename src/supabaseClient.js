cffd230f2c1fdc27a70dfb3619dfbb914540197f
Emmanuel Joshua Badong <136762621+meisems@users.noreply.github.com>
2026-05-19 02:27:25 +0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';        // ← Paste your URL
-const supabaseAnonKey = 'your-anon-public-key-here';               // ← Paste your anon key
+const supabaseUrl = 'https://hcsspkyoomaeciaqmudu.supabase.co';        // ← Paste your URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjc3Nwa3lvb21hZWNpYXFtdWR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NTY1NDIsImV4cCI6MjA5NDUzMjU0Mn0.5wHCjKoXkjKlyp09rr9Yg1SlWTJpGA_If6Ob83LMYPY';               // ← Paste your anon key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
